b64c109a38de4190e5d53f7d1f471fa001956725
Muhammad Abdullah <m.abdullah.fuf@gmail.com>
2024-10-12 18:17:00 +0500

    Basic php work

---
+++ about.php
@@ -0,0 +1,14 @@
+<?php 
+include_once('header.php');
+
+?>
+
+
+<h1>Shop About page</h1>
+
+
+<?php 
+include_once('footer.php');
+
+?>
+    
---
+++ contact.php
@@ -0,0 +1,14 @@
+<?php 
+include_once('header.php');
+
+?>
+
+
+<h1>Shop Contact page</h1>
+
+
+<?php 
+include_once('footer.php');
+
+?>
+    
